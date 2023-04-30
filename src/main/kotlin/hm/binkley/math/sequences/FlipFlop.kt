2bd9eeb805ebc80d327ad97cde40bbc38375518c
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2023-04-30 15:49:23 -0500

    Second test passes

--- src/main/kotlin/hm/binkley/math/sequences/FlipFlop.kt
+++ src/main/kotlin/hm/binkley/math/sequences/FlipFlop.kt
@@ -1,5 +1,6 @@
 package hm.binkley.math.sequences
 
-fun flipFlop(seed: Int) = sequence {
-    yield(seed)
+fun flipFlop(seed: Int) = generateSequence(seed) {
+    val more = 2 * it
+    if (more <= 100) more else 100 - (more - 100)
 }
