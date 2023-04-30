14db86344b8d6f5e7585edb0349d10a058dd3179
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2023-04-30 14:42:42 -0500

    Finally, failing tests

--- src/test/kotlin/hm/binkley/math/sequences/FlipFlopTest.kt
+++ src/test/kotlin/hm/binkley/math/sequences/FlipFlopTest.kt
@@ -3,7 +3,7 @@ package hm.binkley.math.sequences
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
-internal class MathFunctionsTest {
+internal class FlipFlopTest {
     @Test
     fun `should cycle immediately for of seed of 0`() {
         flipFlop(0).take(2).toList() shouldBe listOf(0, 0)
