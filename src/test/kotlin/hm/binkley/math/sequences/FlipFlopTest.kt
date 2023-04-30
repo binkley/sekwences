d0e607a241edf87425a89a04d2bbd3e6243713ba
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2023-04-30 16:33:26 -0500

    Complete test for seed of 1
    Interestingly, mutation testing stays low. Perhaps this needs cycle
    detection and tests for that.

--- src/test/kotlin/hm/binkley/math/sequences/FlipFlopTest.kt
+++ src/test/kotlin/hm/binkley/math/sequences/FlipFlopTest.kt
@@ -11,7 +11,7 @@ internal class FlipFlopTest {
 
     @Test
     fun `should cycle for of seed of 1`() {
-        flipFlop(1).take(8).toList() shouldBe
-            listOf(1, 2, 4, 8, 16, 32, 64, 72)
+        flipFlop(1).take(14).toList() shouldBe
+            listOf(1, 2, 4, 8, 16, 32, 64, 72, 56, 88, 24, 48, 96, 8)
     }
 }
