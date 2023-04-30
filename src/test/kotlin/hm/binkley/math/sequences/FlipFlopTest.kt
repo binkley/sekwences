2228bd925af032533b3e41d06aef3a10e563b473
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2023-04-30 17:07:08 -0500

    Complete test for seed of 100

--- src/test/kotlin/hm/binkley/math/sequences/FlipFlopTest.kt
+++ src/test/kotlin/hm/binkley/math/sequences/FlipFlopTest.kt
@@ -5,25 +5,30 @@ import org.junit.jupiter.api.Test
 
 internal class FlipFlopTest {
     @Test
-    fun `should cycle immediately for of seed of 0`() {
+    fun `should cycle immediately for a seed of 0`() {
         flipFlop(0).take(2).toList() shouldBe listOf(0, 0)
     }
 
     @Test
-    fun `should cycle for of seed of 1`() {
+    fun `should cycle for a seed of 1`() {
         flipFlop(1).take(14).toList() shouldBe
             listOf(1, 2, 4, 8, 16, 32, 64, 72, 56, 88, 24, 48, 96, 8)
     }
 
     @Test
-    fun `should cycle for of seed of 3`() {
+    fun `should cycle for a seed of 3`() {
         flipFlop(3).take(14).toList() shouldBe
             listOf(3, 6, 12, 24, 48, 96, 8, 16, 32, 64, 72, 56, 88, 24)
     }
 
     @Test
-    fun `should cycle for of seed of 5`() {
+    fun `should cycle for a seed of 5`() {
         flipFlop(5).take(6).toList() shouldBe
             listOf(5, 10, 20, 40, 80, 40)
+    }
+
+    @Test
+    fun `should cycle for a seed of 100`() {
+        flipFlop(100).take(2).toList() shouldBe listOf(100, 0)
     }
 }
