d3da50a3560edf782bdd56f4f172bb918930ffcf
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2023-05-01 08:39:24 -0500

    Better demo

--- src/main/kotlin/demo/KotlinMain.kt
+++ src/main/kotlin/demo/KotlinMain.kt
@@ -4,6 +4,12 @@ import hm.binkley.math.sequences.flipFlop
 
 /** Runs the demo. */
 public fun main() {
-    println("An example of \"flip-flop\" with a seed of 1 showing the cycle:")
-    println(flipFlop(1).take(14).toList())
+    println(
+        """
+An example of "flip-flop" with a seed of 1 showing a cycle:
+${flipFlop(1).take(14).toList()}
+An example of "flip-flop" with a seed of 7 showing merging into a seed of 1:
+${flipFlop(7).take(5).toList()}
+        """.trimIndent()
+    )
 }
