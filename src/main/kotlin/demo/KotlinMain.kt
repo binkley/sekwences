f544e46a10fd34456bbb2c541e03aea6429848e4
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2023-05-01 07:50:15 -0500

    Demo shows something

--- src/main/kotlin/demo/KotlinMain.kt
+++ src/main/kotlin/demo/KotlinMain.kt
@@ -1,5 +1,9 @@
 package demo
 
+import hm.binkley.math.sequences.flipFlop
+
 /** Runs the demo. */
 public fun main() {
+    println("An example of \"flip-flop\" with a seed of 1 showing the cycle:")
+    println(flipFlop(1).take(14).toList())
 }
