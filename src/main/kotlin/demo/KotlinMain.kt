25de3cd49ad1f2f01d7e459e317e2c5bd4b38185
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2023-05-10 12:48:57 -0500

    Improve demo

--- src/main/kotlin/demo/KotlinMain.kt
+++ src/main/kotlin/demo/KotlinMain.kt
@@ -1,15 +1,16 @@
 package demo
 
 import hm.binkley.math.sequences.FlipFlop.Companion.flipFlop
+import hm.binkley.math.sequences.findLoop
 
 /** Runs the demo. */
 public fun main() {
     println(
         """
 An example of "flip-flop" with a seed of 1 showing a cycle:
-${flipFlop(1).take(14).toList()}
+${findLoop(flipFlop(1))}
 An example of "flip-flop" with a seed of 7 showing merging into a seed of 1:
-${flipFlop(7).take(5).toList()}
+${findLoop(flipFlop(7))}
         """.trimIndent()
     )
 }
