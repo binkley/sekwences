588be8c95c71b5df6b949bbd6e98cd115ababddc
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2023-07-02 12:37:27 -0500

    Work around kotlin plugin issues

--- src/main/kotlin/demo/KotlinMain.kt
+++ src/main/kotlin/demo/KotlinMain.kt
@@ -4,13 +4,19 @@ import hm.binkley.math.sequences.FlipFlop.Companion.flipFlop
 import hm.binkley.math.sequences.findSelfLoop
 
 /** Runs the demo. */
-public fun main() {
+fun main() {
     println(
         """
 An example of "flip-flop" with a seed of 1 showing a cycle:
 ${findSelfLoop(flipFlop(1))}
 An example of "flip-flop" with a seed of 7 showing merging into a seed of 1:
 ${findSelfLoop(flipFlop(7))}
+        """.trimIndent()
+    )
+    println(
+        """
+67 turns out to be an interesting seed, making a spiral of values:
+${findSelfLoop(flipFlop(67))}
         """.trimIndent()
     )
 }
