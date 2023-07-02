9b2121ca3467c73344e584751703ccf5b2dad525
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2023-07-02 14:29:25 -0500

    Much simpler

--- src/main/kotlin/hm/binkley/math/sequences/FlipFlop.kt
+++ src/main/kotlin/hm/binkley/math/sequences/FlipFlop.kt
@@ -1,13 +1,19 @@
 package hm.binkley.math.sequences
 
-import hm.binkley.math.sequences.FlipFlop.Companion.flipFlop
-
 fun findSelfLoop(
     seed: Int,
     cap: Int = 100,
     compute: (Int) -> Int = { 2 * it }
 ): List<Int> {
-    val itr = flipFlop(seed, cap, compute).iterator()
+    val itr = generateSequence(seed) {
+        val maybe = compute(it)
+        if (maybe < cap) {
+            maybe
+        } else {
+            val overage = maybe - cap
+            cap - overage
+        }
+    }.iterator()
 
     val findSelfLoop = mutableListOf<Int>()
     do {
@@ -19,29 +25,4 @@ fun findSelfLoop(
     } while (itr.hasNext())
 
     return findSelfLoop
-}
-
-class FlipFlop private constructor(
-    val seed: Int,
-    val cap: Int,
-    private val compute: (Int) -> Int,
-) : Sequence<Int> {
-    override fun iterator() = generateSequence(seed) {
-        val maybe = compute(it)
-        if (maybe < cap) {
-            maybe
-        } else {
-            val overage = maybe - cap
-            cap - overage
-        }
-    }.iterator()
-
-    companion object {
-        fun flipFlop(
-            seed: Int,
-            cap: Int = 100,
-            compute: (Int) -> Int = { 2 * it },
-        ) =
-            FlipFlop(seed, cap, compute)
-    }
 }
--- src/test/kotlin/hm/binkley/math/sequences/FlipFlopTest.kt
+++ src/test/kotlin/hm/binkley/math/sequences/FlipFlopTest.kt
@@ -1,68 +1,33 @@
 package hm.binkley.math.sequences
 
-import hm.binkley.math.sequences.FlipFlop.Companion.flipFlop
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
 internal class FlipFlopTest {
     @Test
-    fun `should show the seed`() {
-        flipFlop(99).seed shouldBe 99
-    }
-
-    @Test
-    fun `should show the cap`() {
-        flipFlop(0, 99).cap shouldBe 99
-    }
-
-    @Test
-    fun `should use the compute function`() {
-        flipFlop(33) { it }.take(2).toList() shouldBe listOf(33, 33)
-    }
-
-    @Test
-    fun `should cycle immediately for a seed of 0`() {
-        flipFlop(0).take(2).toList() shouldBe listOf(0, 0)
-    }
-
-    @Test
-    fun `should cycle for a seed of 1`() {
-        flipFlop(1).take(14).toList() shouldBe
-            listOf(1, 2, 4, 8, 16, 32, 64, 72, 56, 88, 24, 48, 96, 8)
-    }
-
-    @Test
-    fun `should cycle for a seed of 3`() {
-        flipFlop(3).take(14).toList() shouldBe
-            listOf(3, 6, 12, 24, 48, 96, 8, 16, 32, 64, 72, 56, 88, 24)
-    }
-
-    @Test
-    fun `should cycle for a seed of 5`() {
-        flipFlop(5).take(6).toList() shouldBe
-            listOf(5, 10, 20, 40, 80, 40)
-    }
-
-    @Test
-    fun `should cycle for a seed of 7`() {
-        flipFlop(7).take(5).toList() shouldBe
-            listOf(7, 14, 28, 56, 88)
-    }
-
-    @Test
-    fun `should cycle for a seed of 50`() {
-        flipFlop(50).take(3).toList() shouldBe listOf(50, 100, 0)
-    }
-
-    @Test
-    fun `should cycle for a seed of 100`() {
-        flipFlop(100).take(2).toList() shouldBe listOf(100, 0)
-    }
-
-    @Test
-    fun `should find loops`() {
+    fun `should find the trivial self loop with the default 2X function`() {
         findSelfLoop(0) shouldBe listOf(0)
+    }
+
+    @Test
+    fun `should find loops with defaults`() {
         findSelfLoop(1) shouldBe
             listOf(1, 2, 4, 8, 16, 32, 64, 72, 56, 88, 24, 48, 96)
+    }
+
+    @Test
+    fun `should find loops with defaults and a non-special seed`() {
+        findSelfLoop(17) shouldBe
+            listOf(17, 34, 68, 64, 72, 56, 88, 24, 48, 96, 8, 16, 32)
+    }
+
+    @Test
+    fun `should find loops with an alternate cap`() {
+        findSelfLoop(1, 4) shouldBe listOf(1, 2, 4, 0)
+    }
+
+    @Test
+    fun `should find loops with an alternative compute function`() {
+        findSelfLoop(1) { 0 } shouldBe listOf(1, 0)
     }
 }
